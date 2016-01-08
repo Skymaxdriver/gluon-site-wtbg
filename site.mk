dff6d3d3001208bd16f24d2e909e613f0935ef32
Kakinger <jan@zueschen.eu>
2016-01-08 10:25:06 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,31 +1,30 @@
 GLUON_SITE_PACKAGES := \
-  	gluon-mesh-batman-adv-15 \
-	gluon-alfred \
-	gluon-announced \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-tunneldigger \
-	gluon-ebtables-filter-roguenets \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-luci-theme \
-	gluon-luci-private-wifi \
-	gluon-next-node \
-	gluon-mesh-vpn-tunneldigger \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	haveged \
-	iptables \
-	iwinfo
+    gluon-mesh-batman-adv-15 \
+        gluon-alfred \
+        gluon-announced \
+        gluon-autoupdater \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-contact-info \
+        gluon-config-mode-core \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-hostname \
+        gluon-config-mode-tunneldigger \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-luci-admin \
+        gluon-luci-autoupdater \
+        gluon-luci-portconfig \
+        gluon-luci-wifi-config \
+        gluon-luci-theme \
+        gluon-luci-private-wifi \
+        gluon-next-node \
+        gluon-mesh-vpn-tunneldigger \
+        gluon-radvd \
+        gluon-setup-mode \
+        gluon-status-page \
+        haveged \
+        iptables \
+        iwinfo
 
 DEFAULT_GLUON_RELEASE := 0.7.6
 
